bd0be6d68684f23c3dafe164f18105aea8b4c4af
Devesh saini <121159484+itsdevcodes@users.noreply.github.com>
2023-06-15 22:45:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+// // //const mainHeading=document.getElementById("main-heading");
+// // //console.log(mainHeading);
+
+// // const mainHeading=document.querySelector("#main-heading");
+// // console.log(mainHeading);
+// // const header = document.querySelectorAll(".header");
+// // console.log(header);
+
+// const mainHeading = document.getElementById("main-heading");
+// console.log(mainHeading.innerText);
+// mainHeading.textContent = "this is a website";
+// console.log(mainHeading.textContent);
+
+// change the style of Element
+
+// const mainHeading = document.querySelector("div.headline h2");
+// console.log(mainHeading);
+
+// style property
+
+//console.log(mainHeading.style); it is a object
+
+// mainHeading.style.color  = "blue";
+// mainHeading.style.backgroundColor = "lightgreen";
+// mainHeading.style.border ="5px solid black";
+
+// get and set attribute
+
+// const link = document.querySelector("a");
+// console.log(link.getAttribute("href"));
+// link.setAttribute("href","https://google.com/");
+// console.log(link.getAttribute("href"))
+// const inputElement =document.querySelector(".todo-form input");
+// console.log(inputElement.getAttribute("type"));
+
+
+// get multipal elements using class name and queryselectorAll
+
+const navItems = document.getElementsByClassName("nav-items");
+console.log(navItems);
+
+
+
